b9fb3d523f8756bddc9b1ba147fb0c4c45206af7
clakg <clakg@icloud.com>
2022-12-18 22:01:47 +0100

    add loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,46 @@
 <template>
   <div>
-    <!-- Movies -->
     <div class="container d-flex align-items-around">
-      <h1 class="title-custom">
-        STEEPLE MOVIES APP
-      </h1>
-      <div class="movies-grid">
-        <div v-for="(movie, index) in movies" :key="index" class="flip-card">
-          <div class="flip-card-inner">
-            <div class="flip-card-front">
-              <img class="image-custom" :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" alt="movies">
-            </div>
-            <div class="flip-card-back">
-              <h3>{{ movie.title }}</h3>
-              <div>
-                <p>Date de parution:<br>{{ movie.release_date }}</p>
-                <hr>
-                <p>Note moyenne:<br>{{ movie.vote_average }}</p>
-                <hr>
-                <p>Genre:<br>{{ movie.genre_ids }}</p>
+      <div class="home">
+        <h1 class="title-custom">
+          STEEPLE MOVIES APP
+        </h1>
+
+        <!-- Search -->
+        <div class="container search">
+          <input
+            v-model.lazy="searchInput"
+            type="text"
+            placeholder="Search"
+            @keyup.enter="$fetch"
+          >
+          <button v-show="searchInput !== ''" class="button" @click="resetSearch">
+            X
+          </button>
+        </div>
+        <div v-if="$fetchState.pending" class="loading">
+          <span>Chargement des vidéos en cours...</span>
+          <br>
+          <span />
+        </div>
+        <div v-else-if="$fetchState.error">
+          <div>Error: {{ $fetchState.error.message }}</div>
+        </div>
+        <div class="movies-grid">
+          <div v-for="(movie, index) in movies" :key="index" class="flip-card">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <img class="image-custom" :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`" alt="movies">
+              </div>
+              <div class="flip-card-back">
+                <h3>{{ movie.title }}</h3>
+                <div>
+                  <p>Date de parution:<br>{{ movie.release_date }}</p>
+                  <hr>
+                  <p>Note moyenne:<br>{{ movie.vote_average }}</p>
+                  <hr>
+                  <p>Genre:<br>{{ movie.genre_ids }}</p>
+                </div>
               </div>
             </div>
           </div>
@@ -35,21 +57,34 @@ export default {
   name: 'IndexPage',
   data () {
     return {
-      movies: []
+      movies: [],
+      searchedMovies: [],
+      searchInput: ''
     }
   },
   async fetch () {
-    await this.fetchMovies()
+    if (this.searchInput === '') {
+      await this.fetchMovies()
+      return
+    }
+    if (this.searchInput !== '') {
+      await this.searchMovies()
+    }
+  },
+  watch: {
+    searchInput () {
+      console.log(this.searchInput)
+    }
   },
   methods: {
+    // get movies from api
     async fetchMovies () {
       const data = await axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=030e4ae4fa04b8499f401b541536d268')
       const result = data
-      // console.log(result.data.results)
       result.data.results.forEach((movie) => {
         this.movies.push(movie)
       })
-    }
+    },
     // async getGenres () {
     // const data = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=030e4ae4fa04b8499f401b541536d268')
     // const result = data
@@ -58,12 +93,52 @@ export default {
     //   this.movies.push(movie)
     // })
     // }
+
+    // get results research from api
+    async searchMovies () {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=030e4ae4fa04b8499f401b541536d268&language=en-US&page=1&query=${this.searchInput}`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.searchedMovies.push(movie)
+      })
+    },
+
+    // initialize searchbar
+    resetSearch () {
+      this.searchInput = ''
+      this.searchedMovies = []
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.home {
+  .loading {
+    padding-top: 120px;
+    align-items: flex-start;
+  }
+  .search {
+    display: flex;
+    padding: 32px 16px;
+    input {
+      max-width: 350px;
+      width: 100%;
+      padding: 12px 6px;
+      font-size: 14px;
+      border: none;
+      &:focus {
+        outline: none;
+      }
+    }
+    .button {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+  }
+}
 .title-custom {
   display: flex;
   justify-content: center;
@@ -139,5 +214,29 @@ export default {
   border-radius: 25px;
   color: white;
   transform: rotateY(180deg);
+}
+
+/* style loader */
+.loading {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @keyframes circle-loading {
+        to {
+            transform: rotateZ('360deg');
+        }
+    }
+    span {
+        display: block;
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+        border: 2px solid transparent;
+        border-top-color: rgb(213, 135, 25);
+        animation: circle-loading 2500ms ease infinite;
+    }
 }
 </style>
